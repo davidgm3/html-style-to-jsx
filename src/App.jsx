30df214db024169106848f56f416226452bcf455
David <dagoma0033@gmail.com>
2023-02-07 13:09:48 +0100

    change method

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,29 @@ function App() {
 			textAreaRef.current.scrollHeight + 'px';
 	};
 
-	function parseInlineStyle(style) {
-		const template = document.createElement('template');
-		template.setAttribute('style', style);
-		return Object.entries(template.style)
-			.filter(([key]) => !/^[0-9]+$/.test(key))
-			.filter(([, value]) => Boolean(value))
-			.reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {});
-	}
+	const formatStringToCamelCase = (str) => {
+		const splitted = str.split('-');
+		if (splitted.length === 1) return splitted[0];
+		return (
+			splitted[0] +
+			splitted
+				.slice(1)
+				.map((word) => word[0].toUpperCase() + word.slice(1))
+				.join('')
+		);
+	};
+	const getStyleObjectFromString = (str) => {
+		const style = {};
+		str.split(';').forEach((el) => {
+			const [property, value] = el.split(':');
+			if (!property) return;
+
+			const formattedProperty = formatStringToCamelCase(property.trim());
+			style[formattedProperty] = value.trim();
+		});
+
+		return style;
+	};
 
 	useEffect(() => {
 		resizeTextArea();
@@ -39,7 +54,9 @@ function App() {
 				></textarea>
 				<h2 className='text-2xl mb-2'>Result</h2>
 				<div className='shadow-md rounded-md p-2 w-full bg-white min-h-[200px]'>
-					<pre>{JSON.stringify(parseInlineStyle(currentInput))}</pre>
+					<pre>
+						{JSON.stringify(getStyleObjectFromString(currentInput))}
+					</pre>
 				</div>
 			</div>
 		</>
